864965f247d0cf30731a8c595dca8b751f565302
Volodymyr <volodymyr.nyshta@gmail.com>
2022-07-17 11:53:58 +0300

    Add HW8 and Task8

---
+++ Program (7).cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Homework8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<Shape> shapes = new List<Shape>();
+        //Console.WriteLine("Let's create figures");
+        //shapes.Add(new Square("Square_A", 2));
+        //shapes.Add(new Square("Square_B", 8));
+        //shapes.Add(new Square("Square_C", 6));
+        //shapes.Add(new Square("Square_D", 4));
+        //shapes.Add(new Circle("Circle_A", 9));
+        //shapes.Add(new Circle("Circle_B", 7));
+        //shapes.Add(new Circle("Circle_C", 5));
+        //shapes.Add(new Circle("Circle_D", 3));
+        start:
+            try
+            {
+                for (int i = 0; i < 2; i++)
+                {
+               
+                    Console.Write("Enter Circle name: ");
+                    string name = Console.ReadLine();
+                    Console.WriteLine("Enter radius: ");
+                    double radius = double.Parse(Console.ReadLine());
+                    shapes.Add(new Circle(name, radius));
+
+                    Console.Write("Enter Square name: ");
+                    string squareName = Console.ReadLine();
+                    Console.WriteLine("Enter side: ");
+                    double side = double.Parse(Console.ReadLine());
+                    shapes.Add(new Square(squareName, side));
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                goto start; 
+            }
+
+            foreach (var item in shapes)
+            {
+                Console.WriteLine($"Figure {item.Name} has perimetr: {item.Perimeter():F2}");
+                Console.WriteLine($"Figure {item.Name} has area: {item.Area():F2}");
+            }
+            double max=shapes[0].Perimeter();
+            int index = 0;
+            for (int i = 0; i < shapes.Count; i++)
+            {
+                if (shapes[i].Perimeter() > max)
+                {
+                    max = shapes[i].Perimeter();
+                    index = i;
+                }
+            }
+            Console.WriteLine("=========================================");
+            Console.WriteLine($"Shape with biggest perimetr is {shapes[index].Name}");
+            Console.WriteLine("=========================================");
+
+
+            Console.WriteLine("============Sorted by area==================");
+            shapes.Sort();
+            foreach (var item in shapes)
+            {
+                Console.WriteLine($"Figure {item.Name} has area: {item.Area():F2}");
+            }
+
+        }
+    }
+}
---
+++ Program (8).cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.IO;
+
+namespace Task
+{
+    abstract class Person : IComparable
+    {
+        private string name;
+        public string Name { get { return name; } }
+        public Person(string name)
+        {
+            this.name = name;
+        }
+        public abstract void Print();
+        public abstract void PrintToFile(StreamWriter stream);
+
+
+        public int CompareTo([AllowNull] Object other)
+        {
+            Person p = other as Person;
+            if (p == null) throw new Exception("Object is not Person");
+            else
+                return this.Name.ToLower().CompareTo(p.Name.ToLower());
+        }
+    }
+    class Staff : Person, IComparable<Staff>
+    {
+        private int salary;
+        public int Salary { get { return salary; } }
+        public Staff(string name, int salary) : base(name)
+        {
+            this.salary = salary;
+        }
+        public override void Print()
+        {
+            Console.WriteLine($"Name: {Name} Salary: {Salary}");
+        }
+        public override void PrintToFile(StreamWriter stream)
+        {
+            stream.WriteLine($"Name: {Name} Salary: {Salary}");
+        }
+
+        public int CompareTo([AllowNull] Staff other)
+        {
+            if (this.Salary > other.Salary)
+            { return 1; }
+            else if (this.Salary < other.Salary)
+            { return -1; }
+            else
+                return 0;
+
+        }
+    }
+    class Teacher : Staff
+    {
+        private string subject;
+        public Teacher(string name, int salary, string subject) : base(name, salary)
+        {
+            this.subject = subject;
+        }
+
+        public override void Print()
+        {
+            Console.WriteLine($"Name: {Name} Salary: {Salary} Subject: {subject}");
+        }
+        public override void PrintToFile(StreamWriter stream)
+        {
+            stream.WriteLine($"Name: {Name} Salary: {Salary} Subject: {subject}");
+        }
+    }
+
+    class Developer : Staff
+    {
+        private int level;
+        public int Level { get { return level; } }
+        public Developer(string name, int salary, int level) : base(name, salary)
+        {
+            this.level = level;
+        }
+        public override void Print()
+        {
+            Console.WriteLine($"Name: {Name} Salary: {Salary} Level: {level}");
+        }
+        public override void PrintToFile(StreamWriter stream)
+        {
+            stream.WriteLine($"Name: {Name} Salary: {Salary} Level: {level}");
+        }
+
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                List<Person> list = new List<Person>();
+                list.Add(new Teacher("Alex", 1000, "C#"));
+                list.Add(new Developer("Oleh", 2500, 5));
+                list.Add(new Staff("Ivan", 500));
+                list.Add(new Teacher("Serhij", 1500, "Java"));
+                list.Add(new Developer("Petro", 2000, 5));
+                list.Add(new Staff("Ivasyk", 700));
+                foreach (var item in list)
+                {
+                    item.Print();
+                }
+                Console.WriteLine("================================");
+                string name = "Alex";
+                bool flag = false;
+                foreach (var item in list)
+                {
+                    if (item.Name == name)
+                    {
+                        item.Print();
+                        flag = true;
+                    }
+                    if (!flag) Console.WriteLine("There are nobody with this name");
+                }
+
+                Console.WriteLine("================================");
+                string path = @"C:\Users\V\Desktop\SoftServe\test.txt";
+                list.Sort();
+                foreach (var item in list)
+                {
+                    item.Print();
+                }
+                Console.WriteLine("================================");
+
+                try
+                {
+                    using (StreamWriter sr = new StreamWriter(path))
+                    {
+                        foreach (var item in list)
+                        {
+                            item.PrintToFile(sr);
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+
+                List<Staff> newList = new List<Staff>();
+                foreach (Staff item in list)
+                {
+                    if (item.GetType() == typeof(Developer) || item.GetType() == typeof(Teacher))
+                    {
+                        newList.Add(item);
+                    }
+                }
+              
+                newList.Sort();
+
+                foreach (var item in newList)
+                {
+                    item.Print();
+                }
+            }
+        }
+    }
+}
+
