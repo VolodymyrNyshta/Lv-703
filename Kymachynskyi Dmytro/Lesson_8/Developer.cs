15567e205d00fece48057444f731a273eaeb4811
Dmytro <dskpochta@gmail.com>
2022-07-19 17:39:50 +0300

    Add Lesson & Homework 8

---
+++ Kymachynskyi Dmytro/Lesson_8/Developer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_8
+{
+    public class Developer : Staff
+    {
+        private string level;
+        public string Level { get { return level; } }
+        public Developer(string firstName, string lastName, int personId, int salary, string level) 
+            : base(firstName, lastName, personId, salary)
+        {
+            this.level = level;
+        }
+        public override void Print()
+        {
+            Console.WriteLine($"Developer: {FirstName} {LastName} Level:{level} ID:{PersonId} ${Salary}");
+        }
+        public override string OutputToString()
+        {
+            return $"Developer: {FirstName} {LastName} Level:{level} ID:{PersonId} ${Salary}";
+        }
+    }
+}
---
+++ Kymachynskyi Dmytro/Lesson_8/Teacher.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_8
+{
+    public class Teacher : Staff
+    {
+        private string subject;
+        public string Subject { get { return subject; }  }
+        public Teacher(string firstName, string lastName, int personId, int salary, string subject) 
+            : base(firstName, lastName, personId, salary)
+        {
+            this.subject = subject;
+        }
+        public override void Print()
+        {
+            Console.WriteLine($"Teacher: {FirstName} {LastName} Subject:{Subject} ID:{PersonId} ${Salary}");
+        }
+        public override string OutputToString()
+        {
+            return $"Teacher: {FirstName} {LastName} Subject:{Subject} ID:{PersonId} ${Salary}";
+        }
+
+    }
+}
