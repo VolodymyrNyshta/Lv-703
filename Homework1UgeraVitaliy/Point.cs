c430b24cf6e649cb9006493dd3126c325ef091ad
UnseenHand <95188996+UnseenHand@users.noreply.github.com>
2022-07-25 17:55:14 +0300

    ADD(Not last version):
    1.Cs main files:
    Homework10Main,
       - Shape,(not in use)
       - Square,(not in use)
       - Circle,(not in use)
       - Triangle,
       - Point
    2.Cs test files:
       - UnitTestTriangle
       - UnitTestPoint(unfinished)

---
+++ Homework1UgeraVitaliy/Point.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace Homework10
+{
+    public class Point
+    {
+        private int x;
+        private int y;
+        public int X
+        {
+            get { return x; }
+        }
+        public int Y
+        {
+            get { return y; }
+        }
+        public Point(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+        public void PointOutput()
+        {
+            Console.WriteLine(ToString());
+        }
+        public override string ToString()
+        {
+            return string.Format("X: {0}, Y: {1}", X, Y);
+        }
+    }
+}
---
+++ Homework1UgeraVitaliy/Triangle.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace Homework10
+{
+    public class Triangle
+    {
+        private Point a;
+        private Point b;
+        private Point c;
+        public Point A
+        {
+            get { return a; }
+        }
+        public Point B
+        {
+            get { return b; }
+        }
+        public Point C
+        {
+            get { return c; }
+        }
+        public Triangle(Point a, Point b, Point c)
+        {
+            this.a = a;
+            this.b = b;
+            this.c = c;
+        }
+        public double DistanceBetweenTwoPoints(Point a, Point b)
+        {
+            return Math.Sqrt(Math.Pow(b.X - a.X, 2) + Math.Pow(b.Y - a.Y, 2));
+        }
+        public double Perimeter()
+        {
+            if ((A.X == B.X && B.X == C.X) || (A.Y == B.Y && B.Y == C.Y))
+            {
+                Console.WriteLine("Your triangle does not exist with inputed points!!!");
+                return 0;
+            }
+            else
+            {
+                return DistanceBetweenTwoPoints(A, C) + DistanceBetweenTwoPoints(B, C) + DistanceBetweenTwoPoints(A, C);
+            }
+        }
+        public double Area()
+        {
+            double halfOfPerimeter = (double)Perimeter() / 2;
+            return Math.Sqrt(halfOfPerimeter * (halfOfPerimeter - DistanceBetweenTwoPoints(A, B)) *
+                (halfOfPerimeter - DistanceBetweenTwoPoints(B, C)) * (halfOfPerimeter - DistanceBetweenTwoPoints(A, C)));
+        }
+        public void TriangleOutput()
+        {
+            Console.WriteLine(ToString());
+        }
+        public override string ToString()
+        {
+            return string.Format($"Triangle values:\nVertex A: ({A.X}, {A.Y})\nVertex B: ({B.X}, {B.Y})\nVertex C: ({C.X}, {C.Y})");
+        }
+    }
+}
