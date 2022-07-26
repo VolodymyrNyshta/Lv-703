e34d7e839ce79e550a52843c66567917d933ae17
Volodymyr <volodymyr.nyshta@gmail.com>
2022-07-26 19:07:29 +0300

    Add HW11

---
+++ Program (12).cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Homework11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student = new Student();
+            Parent parent = new Parent();
+            Accountancy accountancy = new Accountancy();
+
+            MyDel del1 = parent.ResiveMessage;
+            MyDel del2 = accountancy.PayingFellowship;
+            student.MarkChange += del1;
+            student.MarkChange += del2;
+            student.AddMark(71);
+            student.AddMark(65);
+            student.AddMark(71);
+            student.AddMark(90);
+
+            student.MarkChange -= del1;
+            student.AddMark(68);
+        }
+    }
+}
---
+++ Student.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Collections;
+using System.Linq;
+
+namespace Homework11
+{
+     public delegate void MyDel(int m);
+
+    class Accountancy : Student
+    {
+    public void PayingFellowship(int mark)
+        {
+            marks.Add(mark);
+            if (marks.Average() < 71)
+            {
+                Console.WriteLine("Can't resive");
+            }
+            else
+            {
+                Console.WriteLine("Congratulation");
+            }
+
+        }
+    }
+    class Parent : Student
+    { 
+        public void ResiveMessage(int mark)
+        {
+        Console.WriteLine($"Your sun get new mark {mark}");
+        }
+         
+    }
+
+    class Student
+    {
+        private string name;
+        public string Name 
+        { 
+            get { return name; } 
+            set { name = value; } 
+        }
+        public List<int> marks =new List<int>();
+
+        private MyDel markChange;
+        
+        public event MyDel MarkChange 
+        {
+            add 
+            {
+                if (value!=null)
+                {
+                    markChange += value;
+                    Console.WriteLine();
+                }
+            }
+            remove
+            {
+                markChange -= value;
+            }
+         }
+
+        public void AddMark(int x)
+        {
+            marks.Add(x);
+            if (markChange != null)
+            markChange.Invoke(x);
+        }
+    }
+}
