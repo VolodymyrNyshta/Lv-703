4199746f7c62d81a7bfc9e5b9a7543535c8b444b
Vitaliy Smuk <smukvitaliy1993@gmail.com>
2022-07-28 18:29:49 +0400

    VitaliySmuk_HW_11

---
+++ Parent.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VitaliySmuk_HW_11
+{
+    internal class Parent : Student
+    {
+        public void OnMarkChange(int mark)
+        {
+            Console.WriteLine($"Your sons {Name} new mark is {mark}");
+        }
+    }
+}
---
+++ Student.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VitaliySmuk_HW_11
+{
+    public delegate void MyDel(int m);
+
+    internal class Student
+    {
+        private string name;
+        public string Name
+        {
+            get { return this.name = "Vitaliy"; }
+        }
+        public List<int> marks = new List<int>();
+
+        public event MyDel MarkChange;
+        public void AddMark(int newMark)
+        {
+            marks.Add(0);
+            if (MarkChange != null)
+                MarkChange.Invoke(newMark);
+        }
+    }
+}
